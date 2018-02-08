837d171938a01dc4d84e2c8dc6e24086d6688f9a
Ryan Siu <rsiu@stuy.edu>
2018-02-08 14:27:49 -0500

    Finish bounce effect

--- draw.js
+++ draw.js
@@ -1,13 +1,14 @@
 var canvas = document.getElementById("slate");
 var ctx = canvas.getContext("2d");
 var stopButton = document.getElementById("stop");
-var animateButton = document.getElementById("animate");
+var circleButton = document.getElementById("circle");
+var bounceButton = document.getElementById("bounce");
 
 var requestId;
 
 ctx.fillStyle = "lightsteelblue";
 
-var animate = function() {
+var circle = function() {
 
   stop();
 
@@ -33,6 +34,39 @@ var animate = function() {
   drawCircle();
 }
 
+var bounce = function() {
+
+  stop();
+
+  var radius = 50;
+  var x = canvas.width / 2;
+  var y = canvas.height / 2;
+  var dx = 2;
+  var dy = 1;
+
+  var drawBounce = function() {
+    clear();
+    ctx.arc(x, y, radius, 0, 2*Math.PI);
+    ctx.fill();
+    ctx.stroke();
+
+    requestId = window.requestAnimationFrame(drawBounce);
+
+    x += dx;
+    y += dy;
+    console.log(x + ', ' + y);
+
+    if (x >= canvas.width - radius || x <= radius) {
+      dx = -dx + (Math.random() - 0.5);
+    }
+    if (y >= canvas.height - radius || y <= radius) {
+      dy = -dy + (Math.random() - 0.5);
+    }
+  }
+
+  drawBounce();
+}
+
 var clear = function() {
   ctx.clearRect(0, 0, canvas.height, canvas.width);
   ctx.beginPath();
@@ -42,5 +76,6 @@ var stop = function() {
   window.cancelAnimationFrame(requestId);
 }
 
-animateButton.addEventListener('click', animate);
+circleButton.addEventListener('click', circle);
+bounceButton.addEventListener('click', bounce);
 stopButton.addEventListener('click', stop);
